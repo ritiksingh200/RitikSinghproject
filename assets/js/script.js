5ce5fb90b95f0ee7461dceb4cd5edf12e0523635
ritiksingh200 <ritiksingh5590@gmail.com>
2025-09-20 20:20:26 +0530

    css alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // modal variables
 const modal = document.querySelector('[data-modal]');
 const modalCloseBtn = document.querySelector('[data-modal-close]');
+
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
 // modal function
